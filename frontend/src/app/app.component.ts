b0e2e00371ef707bec59da7524a9f10e794c22e7
Daniel Fernandez Ruiz <dfernandez@airzonesl.es>
2020-07-26 13:14:49 +0200

    Backend cargado

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare function js_main();
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'frontend';
+ 
+
+  ngOnInit() {
+    js_main();
+  }
+
 }
